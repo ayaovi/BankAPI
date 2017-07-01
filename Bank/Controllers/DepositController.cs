bf7890f8d36681b4ad765832a692f9ded2f9755d
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-01 12:57:50 +0200

    feat: Add account status validation to DepositController.
    deposit or withdrawal cannot be made to an account that is CLOSED.

--- Bank/Controllers/DepositController.cs
+++ Bank/Controllers/DepositController.cs
@@ -24,17 +24,20 @@ namespace Bank.Controllers
       var account = _accounts.GetAccount(deposit.AccountId);
       if (account == null) return Content(HttpStatusCode.NotFound, new { Message = $"Account with ID: {deposit.AccountId} does not exist." });
       {
-        var amount = deposit.Amount * _rates.GetExchangeRate(deposit.Currency, account.Currency);
-        if (amount < 0 && account.Balance < -amount)
-          return Content(HttpStatusCode.BadRequest, new
-          {
-            Message = $"Insufficient Fund in Account with ID {account.Id}.",
-            AccountBalance = account.Balance,
-            Withdraw = -amount
-          });
-        account.Balance += amount;
-        account.Transactions.Add(new Transaction { Amount = amount, Date = DateTime.Now });
-        return Ok(account);
+        if (account.Status == Status.Closed) return Content(HttpStatusCode.BadRequest, new { Message = $"Account with ID: {deposit.AccountId} is CLOSED." });
+        {
+          var amount = deposit.Amount * _rates.GetExchangeRate(deposit.Currency, account.Currency);
+          if (amount < 0 && account.Balance < -amount)
+            return Content(HttpStatusCode.BadRequest, new
+            {
+              Message = $"Insufficient Fund in Account with ID {account.Id}.",
+              AccountBalance = account.Balance,
+              Withdraw = -amount
+            });
+          account.Balance += amount;
+          account.Transactions.Add(new Transaction {Amount = amount, Date = DateTime.Now});
+          return Ok(account);
+        }
       }
     }
   }
