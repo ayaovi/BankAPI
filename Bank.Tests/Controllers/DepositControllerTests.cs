3a4d201074e2ee58d4a24cbb9f8b0577812d3c93
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-01 11:07:29 +0200

    test: Complete DpeositController Tests

--- Bank.Tests/Controllers/DepositControllerTests.cs
+++ Bank.Tests/Controllers/DepositControllerTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Web.Http.Results;
+using Bank.Controllers;
+using Bank.Models;
 using NUnit.Framework;
 
 namespace Bank.Tests.Controllers
@@ -10,5 +9,35 @@ namespace Bank.Tests.Controllers
   [TestFixture]
   internal class DepositControllerTests
   {
+    [Test]
+    public void Post_GivenDepositAndExistingAccount_ExpectSuccess()
+    {
+      //Arrange
+      new AccountController().Post(new List<Account>
+      {
+        new Account
+        {
+          Id = 1002547,
+          Balance = 25
+        }
+      });
+
+      new ExchangeRateController().Post(new Dictionary<Currency, decimal>
+      {
+        {Currency.Rand, 1},
+        {Currency.Usd, (decimal)12.96},
+        {Currency.Euro, (decimal)14.80}
+      });
+
+      var controller = new DepositController();
+
+      //Act
+      var actionResult = controller.Post(new Deposit { AccountId = 1002547, Amount = 100, Currency = Currency.Euro });
+      var result = new AccountController().Get(1002547);
+
+      //Assert
+      Assert.IsInstanceOf<OkNegotiatedContentResult<Account>>(actionResult);
+      Assert.IsTrue(((OkNegotiatedContentResult<Account>)result).Content.Balance == 1505);
+    }
   }
 }
