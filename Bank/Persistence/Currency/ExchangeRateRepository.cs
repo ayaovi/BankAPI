b54c64f97b191b5d855ff3dbfd3d1cd9a815baa4
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-03 16:14:56 +0200

    feat: Add ExchangeRateRepository.

---
+++ Bank/Persistence/Currency/ExchangeRateRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Bank.Persistence.Common;
+using Dapper;
+
+namespace Bank.Persistence.Currency
+{
+  public class ExchangeRateRepository : IExchangeRateRepository
+  {
+    /// <summary>
+    /// The connection factory
+    /// </summary>
+    private readonly IConnectionStringProvider _connectionStringProvider;
+
+    /// <summary>
+    /// The proc get exchange rate
+    /// </summary>
+    private const string ProcGetExchangeRate = "dbo.pr_GetExchangeRateFromOneCurrencyToAnother";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ExchangeRateRepository"/> class.
+    /// </summary>
+    /// <param name="connectionStringProvider"></param>
+    public ExchangeRateRepository(IConnectionStringProvider connectionStringProvider)
+    {
+      _connectionStringProvider = connectionStringProvider;
+    }
+
+    /// <summary>
+    /// Gets the exchange rate.
+    /// </summary>
+    /// <param name="originCurrency"></param>
+    /// <param name="destinationCurrency"></param>
+    /// <returns></returns>
+    public async Task<IEnumerable<decimal>> GetExchangeRateAsync(Models.Currency originCurrency, Models.Currency destinationCurrency)
+    {
+      using (var connection = new SqlConnection(await _connectionStringProvider.GetConnectionStringAsync()))
+      {
+        await connection.OpenAsync();
+        var parameters = new
+        {
+          OriginCurrencyID = (int) originCurrency,
+          DestinationCurrencyID = (int) destinationCurrency
+        };
+        return await connection.QueryAsync<decimal>(ProcGetExchangeRate, parameters, commandType: CommandType.StoredProcedure);
+      }
+    }
+  }
+}
---
+++ Bank/Persistence/Currency/IExchangeRateRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bank.Persistence.Currency
+{
+  public interface IExchangeRateRepository
+  {
+    /// <summary>
+    /// Gets the exchange rate.
+    /// </summary>
+    /// <param name="originCurrency"></param>
+    /// <param name="destinationCurrency"></param>
+    /// <returns></returns>
+    Task<IEnumerable<decimal>> GetExchangeRateAsync(Models.Currency originCurrency, Models.Currency destinationCurrency);
+
+    //void Save(Models.Currency currency, decimal rate);
+  }
+}
