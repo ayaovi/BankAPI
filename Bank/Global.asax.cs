9513141faf8caf8f817c25dc29fe93343dcae904
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-03 16:30:18 +0200

    chore: Pull in packages.

--- Bank/Global.asax.cs
+++ Bank/Global.asax.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
 
 namespace Bank
 {
-    public class WebApiApplication : System.Web.HttpApplication
+  public class WebApiApplication : System.Web.HttpApplication
+  {
+    protected void Application_Start()
     {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
+      UnityConfig.RegisterComponents();
+      GlobalConfiguration.Configure(WebApiConfig.Register);
+
     }
+  }
 }
