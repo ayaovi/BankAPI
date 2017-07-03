2d3e505e8959ed52e1ff3b7794a91e2269494b15
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-03 16:16:01 +0200

    feat: Add a TransactionRepository.

---
+++ Bank/Persistence/Transaction/ITransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bank.Persistence.Transaction
+{
+  public interface ITransactionRepository
+  {
+    /// <summary>
+    /// Gets the transaction by account ids asynchronously.
+    /// </summary>
+    /// <param name="accountIds">The account ids.</param>
+    /// <returns></returns>
+    Task<IEnumerable<Models.Transaction>> GetTransactionByAccountIdsAsync(IEnumerable<int> accountIds);
+
+    /// <summary>
+    /// Gets the transactions asynchronously.
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<Models.Transaction>> GetTransactionsAsync();
+  }
+}
---
+++ Bank/Persistence/Transaction/TransactionRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Bank.Persistence.Common;
+using Dapper;
+
+namespace Bank.Persistence.Transaction
+{
+  public class TransactionRepository : ITransactionRepository
+  {
+
+    private readonly IConnectionStringProvider _connectionStringProvider;
+
+    /// <summary>
+    /// The proc for getting transaction by account ids.
+    /// </summary>
+    private const string ProcGetTransactionByAccountIds = "dbo.pr_GetTransactionByAccountIds";
+
+    /// <summary>
+    /// The proc for getting all transactions.
+    /// </summary>
+    private const string ProcGetTransactions = "dbo.pr_GetTransactions";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TransactionRepository"/> class.
+    /// </summary>
+    /// <param name="connectionStringProvider"></param>
+    public TransactionRepository(IConnectionStringProvider connectionStringProvider)
+    {
+      _connectionStringProvider = connectionStringProvider;
+    }
+
+    /// <summary>
+    /// Gets the transaction by account ids asynchronously.
+    /// </summary>
+    /// <param name="accountIds">The account ids.</param>
+    /// <returns></returns>
+    public async Task<IEnumerable<Models.Transaction>> GetTransactionByAccountIdsAsync(IEnumerable<int> accountIds)
+    {
+      using (var connection = new SqlConnection(await _connectionStringProvider.GetConnectionStringAsync()))
+      {
+        await connection.OpenAsync();
+        var accountIdsDataTable = Utilities.Util.ToIntTable(accountIds);
+        var parameters = new 
+        {
+          AccountIDs = accountIdsDataTable
+        };
+        return await connection.QueryAsync<Models.Transaction>(ProcGetTransactionByAccountIds, param: parameters, commandType: CommandType.StoredProcedure);
+      }
+    }
+
+    /// <summary>
+    /// Gets the transactions asynchronously.
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IEnumerable<Models.Transaction>> GetTransactionsAsync()
+    {
+      using (var connection = new SqlConnection(await _connectionStringProvider.GetConnectionStringAsync()))
+      {
+        await connection.OpenAsync();
+        return await connection.QueryAsync<Models.Transaction>(ProcGetTransactions, commandType: CommandType.StoredProcedure);
+      }
+    }
+  }
+}
