539adbddd21b312334555251a922b36d03642acd
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-03 16:29:56 +0200

    feat: Move conversion from IEnumerable<int> to IntTable to Utilities.

---
+++ Bank/Persistence/Utilities/Util.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace Bank.Persistence.Utilities
+{
+  public static class Util
+  {
+    /// <summary>
+    /// IEnumerable to the int table type.
+    /// </summary>
+    /// <param name="accountIds">The account ids.</param>
+    /// <returns></returns>
+    public static DataTable ToIntTable(IEnumerable<int> accountIds)
+    {
+      var dataTable = new DataTable("dbo.tp_IntTable");
+      dataTable.Columns.Add("Value", typeof(int));
+
+      foreach (var accountId in accountIds)
+      {
+        dataTable.Rows.Add(accountId);
+      }
+      return dataTable;
+    }
+  }
+}
