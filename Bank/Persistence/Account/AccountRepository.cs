5e9bea4e7458b9e479517de86b1119d2c6db18e9
Ayaovi Djissenou <ayaovi.djissenou@derivco.co.za>
2017-07-03 16:09:05 +0200

    feat: Rearrange AccountRepository location.

---
+++ Bank/Persistence/Account/AccountRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Bank.Persistence.Common;
+using Dapper;
+
+namespace Bank.Persistence.Account
+{
+  public class AccountRepository : IAccountRepository
+  {
+    /// <summary>
+    /// The connection string provider
+    /// </summary>
+    private readonly IConnectionStringProvider _connectionStringProvider;
+
+    /// <summary>
+    /// The proc for getting active accounts.
+    /// </summary>
+    private const string ProcGetActiveAccounts = "dbo.pr_GetActiveAccounts";
+
+    /// <summary>
+    /// The proc get account by account ids
+    /// </summary>
+    private const string ProcGetAccountByAccountIDs = "dbo.pr_GetAccountByAccountIDs";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AccountRepository"/> class.
+    /// </summary>
+    public AccountRepository(IConnectionStringProvider connectionStringProvider)
+    {
+      _connectionStringProvider = connectionStringProvider;
+    }
+
+    /// <summary>
+    /// Gets all accounts asynchronously.
+    /// </summary>
+    /// <param name="accountIds"></param>
+    /// <returns></returns>
+    public async Task<IEnumerable<Models.Account>> GetAccountsAsync(IEnumerable<int> accountIds)
+    {
+      using (var connection = new SqlConnection(await _connectionStringProvider.GetConnectionStringAsync()))
+      {
+        await connection.OpenAsync();
+        var accountIdsDataTable = Utilities.Util.ToIntTable(accountIds);
+        var parameters = new
+        {
+          AccountIDs = accountIdsDataTable
+        };
+        return await connection.QueryAsync<Models.Account>(ProcGetAccountByAccountIDs, parameters, commandType: CommandType.StoredProcedure);
+      }
+    }
+  }
+}
---
+++ Bank/Persistence/Account/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Bank.Persistence.Account
+{
+  public interface IAccountRepository
+  {
+    /// <summary>
+    /// Gets all accounts asynchronously.
+    /// </summary>
+    /// <param name="accountIds"></param>
+    /// <returns></returns>
+    Task<IEnumerable<Models.Account>> GetAccountsAsync(IEnumerable<int> accountIds);
+  }
+}
